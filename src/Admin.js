89172f4287473c01e69d8053b35a498612d99c32
Aslam Rasheed <aslampr07@outlook.com>
2019-09-29 21:09:26 +0530

    Basic framework

--- src/Admin.js
+++ src/Admin.js
@@ -4,23 +4,78 @@ import React from 'react';
 export default class Admin extends React.Component {
 
 
-  constructor(props){
+  constructor(props) {
     super(props)
+
 
     this.increment = this.increment.bind(this)
 
     this.channel = new BroadcastChannel("score");
+    this.state = {
+      teamA: 0,
+      teamB: 0
+    }
   }
 
-  increment(){
-    this.channel.postMessage("This is from admin side")
+  increment(team) {
+
+    let teamA = this.state.teamA 
+    let teamB = this.state.teamB
+
+    switch (team) {
+      case "A": 
+      teamA = this.state.teamA + 1
+      this.setState({
+        teamA: teamA
+      })
+        break;
+      case "B": 
+      teamB = this.state.teamB + 1
+      this.setState({
+        teamB: teamB
+      })
+        break;
+    }
+
+    this.channel.postMessage({ teamA, teamB })
+
+  }
+
+  decrement(team) {
+
+    let teamA = this.state.teamA 
+    let teamB = this.state.teamB
+    switch (team) {
+      case "A": 
+      teamA = this.state.teamA - 1
+      this.setState({
+        teamA: teamA
+      })
+        break;
+      case "B": 
+      teamB = this.state.teamB - 1
+      this.setState({
+        teamB: teamB
+      })
+        break;
+    }
+
+    this.channel.postMessage({ teamA, teamB })
   }
 
   render() {
     return (
       <div>
-        <h1>Admin panel</h1>
-        <button onClick={this.increment}>Increase</button>
+        <div>
+          <button onClick={() => this.increment("A")}>+</button>
+          <span>{this.state.teamA}</span>
+          <button onClick={() => this.decrement("A")}>-</button>
+        </div>
+        <div>
+          <button onClick={() => this.increment("B")}>+</button>
+          <span>{this.state.teamB}</span>
+          <button onClick={() => this.decrement("B")}>-</button>
+        </div>
       </div>
     )
   }
--- src/ScoreBoard.js
+++ src/ScoreBoard.js
@@ -5,21 +5,29 @@ export default class ScoreBoard extends React.Component {
     constructor(props) {
         super(props)
 
-
-        // this.messageRecieved = this.messageRecieved.bind(this)
+        this.messageRecieved = this.messageRecieved.bind(this)
 
         this.channel = new BroadcastChannel("score");
         this.channel.onmessage = this.messageRecieved;
+
+        this.state = {
+            teamA: 0,
+            teamB: 0
+        }
     }
 
-    messageRecieved(e){
-        alert(e.data)
+    messageRecieved(e) {
+        this.setState(e.data)
     }
 
 
     render() {
         return (
-            <h1>Scoreboard</h1>
+            <div>
+                <h1>Scoreboard</h1>
+                <h2>Team A: {this.state.teamA}</h2>
+                <h2>Team B: {this.state.teamB}</h2>
+            </div>
         )
     }
 }
